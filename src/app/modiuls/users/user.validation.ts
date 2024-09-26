cb796c3087c7aff15ef16afbe847fe84251441b3
Alifkhan11 <mdalifk2002@gmail.co>
2024-09-26 20:38:41 +0600

    allow any link

--- src/app/modiuls/users/user.validation.ts
+++ src/app/modiuls/users/user.validation.ts
@@ -7,11 +7,6 @@ export const createUserValidationSchema = z.object({
   password: z.string().min(8),
   role: z.enum(["user", "seller"]).default("user"),
   isDeleted: z.boolean().default(false),
-  phone: z.number(),
-  division: z.string(),
-  distric: z.string(),
-  upzala: z.string(),
-  detailsAddress: z.string(),
 });
 
 export const createUserInfoValidationschema = z.object({
